522e893e2fa4b54672a81ef5feea9c8c5813e44c
mstaso <mstaso1@gmail.com>
2020-07-15 18:36:54 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
+
+
 console.log('%c HI', 'color: firebrick')
+
+document.addEventListener('DOMContentLoaded', function () {
+fetch ("https://dog.ceo/api/breeds/image/random/4").then(function(response) {
+    return response.json();
+  })
+  .then(function(json){
+  })
+});
+
