92673480b9635d1406ee7caad1ad89444fb2d544
shadman-a <shadman@outlook.com>
2020-07-17 08:38:47 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,2 +1,18 @@
 console.log('%c HI', 'color: firebrick')
-sdfsfasf
+
+// const toyList = document.getElementById("dog-image-container");
+fetch('https://dog.ceo/api/breeds/image/random/4')
+  .then(response => response.json())
+  .then(data => {
+    data.message.forEach(toy => {
+      renderToys(toy);
+    });
+  })
+
+
+function renderToys(toy){
+    let container = document.querySelector('#dog-image-container');
+    let newImageEl = document.createElement('img');
+    newImageEl.src = toy;
+    container.appendChild(newImageEl);
+}
